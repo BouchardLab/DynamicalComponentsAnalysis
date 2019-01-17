3ea6c1212d6f65766ed1ae9dc8d77c7434e37705
David Clark <davidclark@berkeley.edu>
2019-01-16 22:51:37 -0800

    Clean up cca.py a little bit

--- cca.py
+++ cca.py
@@ -4,112 +4,90 @@ import scipy.optimize
 import scipy.stats
 from autograd import grad
 
-def calc_pi(X, T):
-
-	#mean-center data
-	X = X - np.mean(X, axis=0)
-
-	#Compute N-by-N cross-covariance matrices for all 0<=delta_t<=2*T-1
-	N = X.shape[1]
-	cross_cov_mats = np.zeros((2*T, N, N))
-	for delta_t in range(2*T):
-	    cross_cov = np.dot(X[delta_t:].T, X[:len(X)-delta_t])/(len(X) - delta_t - 1)
-	    cross_cov_mats[delta_t] = cross_cov
-
-	vals = []
-	for i in range(2*T):
-		for j in range(2*T):
-			C = cross_cov_mats[int(np.abs(i-j))]
-			vals.append(C)
-
-	autocov_2T_tensor = np.reshape(np.array(vals), (2*T, 2*T, N, N))
-	autocov_2T = np.concatenate([np.concatenate(autocov_ii, axis=1) for autocov_ii in autocov_2T_tensor])
-	autocov_T = autocov_2T[:T * N, :T * N]
-
-	sgn_T, logdet_T = np.linalg.slogdet(autocov_T)
-	sgn_2T, logdet_2T = np.linalg.slogdet(autocov_2T)
-
-	PI = 2*logdet_T - logdet_2T
-	return PI
-
-
-
-
-
-
-def build_loss(X, T, latent_dim, kron=False):
-        
+def calc_cross_cov_mats(X, delta_t_max):
+    
     #mean-center data
     X = X - np.mean(X, axis=0)
-    
-    #Compute N-by-N cross-covariance matrices for all 0<=delta_t<=2*T-1
+
+    #Compute N-by-N cross-covariance matrices for all 0<=delta_t<=delta_t_max-1
     N = X.shape[1]
-    cross_cov_mats = np.zeros((2*T, N, N))
-    for delta_t in range(2*T):
+    cross_cov_mats = np.zeros((2*delta_t_max, N, N))
+    for delta_t in range(2*delta_t_max):
         cross_cov = np.dot(X[delta_t:].T, X[:len(X)-delta_t])/(len(X) - delta_t - 1)
         cross_cov_mats[delta_t] = cross_cov
         
-    #Compute PI using big matrices + Kronecker products
-    if kron:
-        #Compute 2*T*N-by-2*T*N cross-covariance matrix
-        C_2T = np.zeros((2*T*N, 2*T*N))
-        for delta_t in range(2*T):
-            #upper diagonal
-            C_2T[:2*T*N-delta_t*N, delta_t*N:] += np.kron(np.eye(2*T - delta_t), cross_cov_mats[delta_t])
-
-            #upper diag = lower diag if delta_t == 0
-            if delta_t == 0:
-                continue
-
-            #lower-diagonal
-            C_2T[delta_t*N:, :2*T*N-delta_t*N] += np.kron(np.eye(2*T - delta_t), cross_cov_mats[delta_t])
-
-        #Get T*N-by-T*N cross-covariance matrix
-        C_T = C_2T[:T*N, :T*N]
-
-        #Define loss functon
-        def loss(v):
-
-            v_column = np.reshape(v, (len(v), 1))
-            v_norm = v_column / np.sqrt(np.sum(v_column**2))
-
-            V_T = np.kron(np.eye(T), v_norm)
-            V_2T = np.kron(np.eye(2*T), v_norm)
-
-            autocov_T = np.dot(V_T.T, np.dot(C_T, V_T))
-            autocov_2T = np.dot(V_2T.T, np.dot(C_2T, V_2T))
-
-            sgn_T, logdet_T = np.linalg.slogdet(autocov_T)
-            sgn_2T, logdet_2T = np.linalg.slogdet(autocov_2T)
-
-            PI = 2*logdet_T - logdet_2T
-            return -PI #Loss function (low negative PI = high PI)
-
-        print("Kron loss!")
-        return loss
+    return cross_cov_mats
     
-    #Compute PI using a Python list --> matrix
-    else:
-        def loss(v):
-            vals = []
-            N = X.shape[1]
-            v2d_notnorm = v.reshape(latent_dim, N)
-            v2d = v2d_notnorm / np.sqrt(np.sum(v2d_notnorm**2, axis=1, keepdims=True))
-            for i in range(2*T):
-                for j in range(2*T):
-                    C = cross_cov_mats[int(np.abs(i-j))]
-                    val = np.dot(v2d, np.dot(C, v2d.T))
-                    vals.append(val)
 
-            autocov_2T_tensor = np.reshape(np.array(vals), (2*T, 2*T, latent_dim, latent_dim))
-            autocov_2T = np.concatenate([np.concatenate(autocov_ii, axis=1) for autocov_ii in autocov_2T_tensor])
-            autocov_T = autocov_2T[:T * latent_dim, :T * latent_dim]
+def calc_pi(X, T):
+
+    cross_cov_mats = calc_cross_cov_mats(X, 2*T)
+    N = X.shape[1]
+
+    cross_cov_mats_repeated = []
+    for i in range(2*T):
+        for j in range(2*T):
+            cross_cov = cross_cov_mats[int(np.abs(i-j))]
+            cross_cov_mats_repeated.append(cross_cov)
+
+    cov_2T_tensor = np.reshape(np.array(cross_cov_mats_repeated), (2*T, 2*T, N, N))
+    cov_2T = np.concatenate([np.concatenate(cov_ii, axis=1) for cov_ii in cov_2T_tensor])
+    cov_T = cov_2T[:T*N, :T*N]
+
+    sgn_T, logdet_T = np.linalg.slogdet(cov_T)
+    sgn_2T, logdet_2T = np.linalg.slogdet(cov_2T)
+    PI = 2*logdet_T - logdet_2T
+    
+    return PI
+
+
+def build_loss(X, T, d):
             
-            sgn_T, logdet_T = np.linalg.slogdet(autocov_T)
-            sgn_2T, logdet_2T = np.linalg.slogdet(autocov_2T)
-            
-            PI = 2*logdet_T - logdet_2T
-            return -PI
+    cross_cov_mats = calc_cross_cov_mats(X, 2*T)
+    N = X.shape[1]
+    
+    def loss(V_flat):
         
-        print("List loss!")
-        return loss
+        #Reshape and normalize V_flat
+        V_unnormalized = V_flat.reshape(N, d)
+        V = V_unnormalized / np.sqrt(np.sum(V_unnormalized**2, axis=0))
+        
+        cross_cov_mats_repeated = []
+        for i in range(2*T):
+            for j in range(2*T):
+                cross_cov = cross_cov_mats[int(np.abs(i-j))]
+                cross_cov_proj = np.dot(V.T, np.dot(cross_cov, V))
+                cross_cov_mats_repeated.append(cross_cov_proj)
+
+        cov_2T_tensor = np.reshape(np.array(cross_cov_mats_repeated), (2*T, 2*T, d, d))
+        cov_2T = np.concatenate([np.concatenate(cov_ii, axis=1) for cov_ii in cov_2T_tensor])
+        cov_T = cov_2T[:T*d, :T*d]
+
+        sgn_T, logdet_T = np.linalg.slogdet(cov_T)
+        sgn_2T, logdet_2T = np.linalg.slogdet(cov_2T)
+        PI = 2*logdet_T - logdet_2T
+
+        return -PI
+
+    return loss
+
+
+def run_cca(X, T, d):
+    
+    loss = build_loss(X, T, d)
+    grad_loss = grad(loss)
+    
+    N = X.shape[1]
+    V_init = np.random.normal(0, 1, (N, d))
+    V_init = V_init / np.sqrt(np.sum(V_init**2, axis=0))
+
+    opt_result = scipy.optimize.minimize(loss, V_init.flatten(), method='BFGS', jac=grad_loss)
+    V_opt_flat = opt_result["x"]
+    V_opt = V_opt_flat.reshape((N, d))
+    V_opt = V_opt / np.sqrt(np.sum(V_opt**2, axis=0))
+    
+    return V_opt
+
+
+
+

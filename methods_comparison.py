b9cf6012d70b22984a6d04bbf3a71e561d0e4df9
Jesse Livezey <jesse.livezey@gmail.com>
2019-02-03 14:09:51 -0800

    added docstrings

--- methods_comparison.py
+++ methods_comparison.py
@@ -2,11 +2,25 @@ import numpy as np
 
 
 class SlowFeatureAnalysis(object):
+    """Slow Feature Analysis (SFA)
+
+    Parameters
+    ----------
+    n_components : int
+        The number of components to learn.
+    """
     def __init__(self, n_components):
         self.n_components = n_components
         self.coef_ = None
 
     def fit(self, X):
+        """Fit the SFA model.
+
+        Parameters
+        ----------
+        X : ndarray (features, time)
+            Data to fit SFA model to.
+        """
         X_stan = X - X.mean(axis=1, keepdims=True)
         uX, sX, vhX = np.linalg.svd(X_stan)
         whiten = np.diag(1./sX).dot(uX.T)
@@ -17,10 +31,24 @@ class SlowFeatureAnalysis(object):
         self.coef_ = proj.dot(whiten)[::-1][:self.n_components]
 
     def transform(self, X):
+        """Transform the data according to the fit SFA model.
+
+        Parameters
+        ----------
+        X : ndarray (features, time)
+            Data to transform using the SFA model.
+        """
         if self.coef_ is None:
             raise ValueError
         return self.coef_.dot(X)
 
     def fit_transform(self, X):
+        """Fit the SFA model and transform the features.
+
+        Parameters
+        ----------
+        X : ndarray (features, time)
+            Data to fit SFA model to and then transformk.
+        """
         self.fit(X)
         return self.coef_.dot(X)

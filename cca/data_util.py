e4106bc067eb24c9cd1ac1a5cb929e06dfa131d9
Jesse Livezey <jlivezey@lbl.gov>
2019-05-14 16:06:55 -0700

    updates

--- cca/data_util.py
+++ cca/data_util.py
@@ -78,14 +78,14 @@ def load_mocap_data(filename, z_score=True):
 	return angles
 
 
-def load_sabes_data(filename, bin_width_s=0.1, session=None, min_spike_count=1000):
-    f = h5py.File(filename, "r")
-    sessions = list(f.keys())
-    lengths = np.array([f[session]["M1"]["spikes"].shape[0] for session in sessions])
-    if session is None:
-        #use longest session if none is provided
-        session = sessions[np.argsort(lengths)[::-1][0]]
-    X, Y = f[session]["M1"]["spikes"], f[session]["cursor"]
+def load_sabes_data(filename, bin_width_s=0.05, session=None, min_spike_count=1000):
+    with h5py.File(filename, "r") as f:
+        sessions = list(f.keys())
+        lengths = np.array([f[session]["M1"]["spikes"].shape[0] for session in sessions])
+        if session is None:
+            #use longest session if none is provided
+            session = sessions[np.argsort(lengths)[::-1][0]]
+        X, Y = f[session]["M1"]["spikes"][:], f[session]["cursor"][:]
     chunk_size = int(np.round(bin_width_s / .004)) #4 ms default bin width
     X, Y = sum_over_chunks(X, chunk_size), sum_over_chunks(Y, chunk_size)/chunk_size
     X = X[:, np.sum(X, axis=0) > min_spike_count]
